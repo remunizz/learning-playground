1e04f87b90188df97734dfa356435db10359c258
Renan Muniz <renan@renanmuniz.com>
2016-08-05 02:53:20 -0300

    Add hexadecimal-encoding program.js, contains the hexadecimal-encoding folder and the node program.js exercise

---
+++ bytewiser/hexadecimal-encoding/program.js
@@ -0,0 +1,19 @@
+// Store the program arguments
+var argvs = process.argv.slice(2);
+
+// Create the final string
+var hexLabel = '';
+
+// Instantiate the buffer
+var buffer;
+
+// Run a loop of all arguments
+argvs.forEach((argv) => {
+  // Creare a new buffer with an argument hex value
+  buffer = new Buffer(Number(argv).toString(16));
+  // Convert the buffer to string
+  hexLabel += buffer.toString();
+}); 
+
+// Print the result
+console.log(hexLabel);
