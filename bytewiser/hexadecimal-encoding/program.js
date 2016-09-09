3731d541387103dae1141d67572bad73b9c9ede2
Renan Muniz <renan@renanmuniz.com>
2016-09-09 11:15:22 -0300

    Update hexadecimal-encoding and modifying-buffers, update code indentation

--- bytewiser/hexadecimal-encoding/program.js
+++ bytewiser/hexadecimal-encoding/program.js
@@ -9,10 +9,10 @@ var buffer;
 
 // Run a loop of all arguments
 argvs.forEach((argv) => {
-  // Creare a new buffer with an argument hex value
-  buffer = new Buffer(Number(argv).toString(16));
-  // Convert the buffer to string
-  hexLabel += buffer.toString();
+	// Creare a new buffer with an argument hex value
+	buffer = new Buffer(Number(argv).toString(16));
+	// Convert the buffer to string
+	hexLabel += buffer.toString();
 }); 
 
 // Print the result
--- bytewiser/modifying-buffers/program.js
+++ bytewiser/modifying-buffers/program.js
@@ -1,32 +1,32 @@
 // Listen to stdin event
 process.stdin.on('readable', ()=> {
 
-  // Read the stdin and store the result in buffer var
-  var buffer = process.stdin.read();
+	// Read the stdin and store the result in buffer var
+	var buffer = process.stdin.read();
 
-  // Check if buffer is null
-  if (buffer !== null) {
+	// Check if buffer is null
+	if (buffer !== null) {
 
-    // Create loop vars
-	var i = 0;
-	var max = buffer.length;
+		// Create loop vars
+		var i = 0;
+		var max = buffer.length;
 
-	// Loop the buffer
-	for (i; i < max; i++) {
+		// Loop the buffer
+		for (i; i < max; i++) {
 
-      // Check if the buffer is "."
-	  if (buffer[i] === 46) {
-        
-        // Change the buffer item to "!"
-        buffer[i] = 33;
-	  }
+			// Check if the buffer is "."
+			if (buffer[i] === 46) {
+
+				// Change the buffer item to "!"
+				buffer[i] = 33;
+			}
+		}
+
+		// Print the buffer
+		console.log(buffer);
+
+		// End the stdin
+		process.stdin.end();
 	}
-
-	// Print the buffer
-    console.log(buffer);
-
-    // End the stdin
-	process.stdin.end();
-  }
 });
 
